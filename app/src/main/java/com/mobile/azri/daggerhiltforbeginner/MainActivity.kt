84c90a748cd89e80589635ef77dc026c09b0a5ba
azrinurvani <azrinurvani@gmail.com>
2021-08-16 10:35:34 +0700

    Step 2 : - Menunjukkan contoh issue yang ada pada Inject Constructor (ex: Inject Class Interface pada Class depedency yang akan digunakakan pada Class @AndroidEntryPoint)

--- app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MainActivity.kt
+++ app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MainActivity.kt
@@ -2,9 +2,13 @@ package com.mobile.azri.daggerhiltforbeginner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
 
+//yang menggunakan depedency itu diberikan annotation @AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
@@ -12,16 +16,34 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var injectSomeClass: SomeClass
 
+//    @Inject
+//    lateinit var injectInterfaceImpl: SomeClassDepedency
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         println(injectSomeClass.doAThing())
         println(injectSomeClass.doSomeOtherThing())
+
+//        println(injectInterfaceImpl.doInterfaceImpl())
     }
 }
 
+@AndroidEntryPoint
+class MyFragment : Fragment(){
+
+    @Inject
+    lateinit var someClass : SomeClass
+}
+
+
+//TODO 5 - Buat depedency agar dipanggil pada class lain (ex:MainActivity)
 //ini dinamakan constructor injection
+//TODO 6 - Tambahkan scope ActivityScope
+//karena digunakan pada Activity, maka pada inject yang ada di MyFragment tidak terjadi error
+//dikarenakan Tier Activity lebih tinggi dari Fragment, sehingga masih bisa digunakan pada Activity dan Fragment
+@ActivityScoped
 class SomeClass @Inject constructor(
     private val someOtherClass: SomeOtherClass
 ){
@@ -35,9 +57,36 @@ class SomeClass @Inject constructor(
     }
 }
 
+//Pembuatan depedency cukup menggunakan annotation @Inejct constructor pada class sebagai penyedia depdency (Provider)
+@ActivityScoped
 class SomeOtherClass @Inject constructor(){
 
     fun doSomeOtherThing(): String{
         return "Look I did Some Other Thing"
     }
 }
+
+
+/*Bagian ini menunjukkan issue pada Inject Constructor*/
+//@ActivityScoped
+//class SomeClassDepedency @Inject constructor(
+//    private val someInterface: SomeInterface
+//){
+//    fun doInterfaceImpl(): String{
+//        return someInterface.exampleInterface()
+//    }
+//}
+//
+////Ini tidak bisa diterapkan (akan error), karena interface ticdak bisa di implement di injection constructor,
+///Mesti dibuatkan suatu module @Binds agar Interface sudah dinyatakan bisa di build menggunakan Hilt
+//class SomeInterfaceImpl @Inject constructor() : SomeInterface{
+//    override fun exampleInterface():String {
+//        return "Interface implement"
+//    }
+//
+//}
+//
+//interface SomeInterface{
+//
+//    fun exampleInterface():String
+//}
--- app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MyApp.kt
+++ app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MyApp.kt
@@ -3,6 +3,9 @@ package com.mobile.azri.daggerhiltforbeginner
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//TODO 3 - Buat class extend Application dengan annotation @HiltAndroidApp
+//Pada bagian ini akan generate Semua Component yang dibutuhkan pada setiap pembuatan depedency seperti
+//AppComponent, ActivityComponent, FragmentComponent
 @HiltAndroidApp
 class MyApp : Application(){
 }
