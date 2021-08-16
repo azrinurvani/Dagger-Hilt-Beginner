a42eb8708d011778754470905fa45380af28007f
azrinurvani <azrinurvani@gmail.com>
2021-08-16 11:52:36 +0700

    Step 3 : - Implementasi @Binds dan @Provides pada pembuatan module (@Module) agar interface dan object dari third-party atau library bisa di Inject

--- app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MainActivity.kt
+++ app/src/main/java/com/mobile/azri/daggerhiltforbeginner/MainActivity.kt
@@ -3,10 +3,19 @@ package com.mobile.azri.daggerhiltforbeginner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.android.scopes.FragmentScoped
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
+import javax.inject.Singleton
 
 //yang menggunakan depedency itu diberikan annotation @AndroidEntryPoint
 @AndroidEntryPoint
@@ -16,8 +25,8 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var injectSomeClass: SomeClass
 
-//    @Inject
-//    lateinit var injectInterfaceImpl: SomeClassDepedency
+    @Inject
+    lateinit var injectInterfaceImpl: SomeClassDepedency
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         println(injectSomeClass.doAThing())
         println(injectSomeClass.doSomeOtherThing())
 
-//        println(injectInterfaceImpl.doInterfaceImpl())
+        println(injectInterfaceImpl.doInterfaceImpl())
     }
 }
 
@@ -57,7 +66,7 @@ class SomeClass @Inject constructor(
     }
 }
 
-//Pembuatan depedency cukup menggunakan annotation @Inejct constructor pada class sebagai penyedia depdency (Provider)
+//Pembuatan depedency cukup menggunakan annotation @Inject constructor pada class sebagai penyedia depdency
 @ActivityScoped
 class SomeOtherClass @Inject constructor(){
 
@@ -67,26 +76,67 @@ class SomeOtherClass @Inject constructor(){
 }
 
 
-/*Bagian ini menunjukkan issue pada Inject Constructor*/
-//@ActivityScoped
-//class SomeClassDepedency @Inject constructor(
-//    private val someInterface: SomeInterface
-//){
-//    fun doInterfaceImpl(): String{
-//        return someInterface.exampleInterface()
-//    }
-//}
-//
+@ActivityScoped
+class SomeClassDepedency @Inject constructor(
+    private val someInterface: SomeInterface
+){
+    fun doInterfaceImpl(): String{
+        return someInterface.exampleInterface()
+    }
+}
+
 ////Ini tidak bisa diterapkan (akan error), karena interface ticdak bisa di implement di injection constructor,
 ///Mesti dibuatkan suatu module @Binds agar Interface sudah dinyatakan bisa di build menggunakan Hilt
-//class SomeInterfaceImpl @Inject constructor() : SomeInterface{
-//    override fun exampleInterface():String {
-//        return "Interface implement"
-//    }
+class SomeInterfaceImpl @Inject constructor(
+    private val someDepedency:String
+) : SomeInterface{
+    override fun exampleInterface():String {
+        return "Interface implement : $someDepedency"
+    }
+
+}
+
+interface SomeInterface{
+
+    fun exampleInterface():String
+}
+
+//TODO 7 Buat module untuk depedency, lebih diutamakan menggunakan @Provides daripada @Binds
+//Module yang digunakan untuk Binds Inteface
+//SingletonComponent maksudnya adalah ApplicationComponent, yang mana module dibuatkan dengan scope Singleton
+// atau pada level Application, bisa diganti dengan scope ActivityComponent, dll, namun function atau depdency pada module
+// mesti menyesuaikan
+//@InstallIn(ActivityComponent::class)
+//@Module
+//abstract class MyModule{
 //
+//    @ActivityScoped
+//    @Binds
+//    abstract fun bindInterfaceDepedency(someInterfaceImpl: SomeInterfaceImpl):SomeInterface
 //}
-//
-//interface SomeInterface{
-//
-//    fun exampleInterface():String
-//}
+
+@InstallIn(ActivityComponent::class)
+@Module
+class MyModule{
+
+    @ActivityScoped
+    @Provides
+    fun provideSomeString():String{
+        return "some string"
+    }
+
+    @ActivityScoped
+    @Provides
+    fun provideInterfaceDepedency(
+        someString:String
+    ):SomeInterface{
+        return SomeInterfaceImpl(someString)
+    }
+
+    @ActivityScoped
+    @Provides
+    fun providesGsonDepedency():Gson{
+        return Gson()
+    }
+
+}
